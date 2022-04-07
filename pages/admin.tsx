a7504259bc2f4fee0ca810baef9e242cd8f19af9
unknown <sjjoseph106@gmail.com>
2022-04-07 17:34:10 +0900

    Feat: Header 추가

---
+++ pages/admin.tsx
@@ -0,0 +1,10 @@
+import Seo from '../components/Seo';
+
+export default function Admin() {
+  return (
+    <>
+      <Seo title={'Admin'} />
+      <h1>Admin</h1>
+    </>
+  );
+}
---
+++ pages/project.tsx
@@ -0,0 +1,10 @@
+import Seo from '../components/Seo';
+
+export default function Project() {
+  return (
+    <div>
+      <Seo title={'Project'} />
+      <h1>Project</h1>
+    </div>
+  );
+}

391e7bb826b156bb6172ac25e37ac94c547d27c2
unknown <sjjoseph106@gmail.com>
2022-04-13 15:58:04 +0900

    Chore: styled-components 반응형 테스트(2)

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,10 +1,20 @@
 import Seo from '../components/Seo';
+import styled from 'styled-components';
+import { media } from '../styles/theme';
+
+const Test = styled.div`
+  color: ${({ theme }) => theme.color.primary};
+  ${media.mobile} {
+    color: ${({ theme }) => theme.color.secondary};
+  }
+`;
 
 export default function Admin() {
   return (
     <>
       <Seo title={'Admin'} />
-      <h1>Admin</h1>
+      <Test className="pc-only">PC</Test>
+      <Test className="mobile-only">Mobile</Test>
     </>
   );
 }
--- pages/project.tsx
+++ pages/project.tsx
@@ -2,9 +2,9 @@ import Seo from '../components/Seo';
 
 export default function Project() {
   return (
-    <div>
+    <>
       <Seo title={'Project'} />
-      <h1>Project</h1>
-    </div>
+      Project
+    </>
   );
 }

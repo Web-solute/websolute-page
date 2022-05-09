ee8d8827d910c9f198ee9ef9b02f259fa2f245e4
sinbox0701 <psh090953@gmail.com>
2022-05-09 16:59:33 +0900

    Feat: 백엔드 개발 환경 세팅 DB: PlanetSacle(MySQL) + Prisma Setup api Setup (백엔드 API 설계용 폴더)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,18 @@ import Layout from '../components/Layout';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../styles/global-style';
 import { theme } from '../styles/theme';
+import { SWRConfig } from 'swr';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SWRConfig value={{ fetcher: (url: string) => fetch(url).then((response) => response.json()) }}>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
-    </>
+    </SWRConfig>
   );
 }
 

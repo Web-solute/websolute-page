678de323ced63cb5e9f471cdc92ff2b05a001eb6
unknown <sjjoseph106@gmail.com>
2022-06-05 17:44:22 +0900

    Feat: 텍스트 슬라이드업 캐러셀

--- components/Home/HomeAnimation.styled.tsx
+++ components/Home/HomeAnimation.styled.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { media } from '../../styles/theme';
 
 export const AnimationContainer = styled.div`
@@ -6,7 +6,7 @@ export const AnimationContainer = styled.div`
 `;
 
 export const LogoText = styled.div`
-  font-size: clamp(3.5rem, 9.5vw, 7rem);
+  font-size: clamp(3.5rem, 8vw, 7.5rem);
   background: linear-gradient(
     to right bottom,
     ${(props) => props.theme.color.secondary},
@@ -18,10 +18,49 @@ export const LogoText = styled.div`
   background-clip: text;
   font-family: 'Poppins', sans-serif;
   font-weight: bold;
-  padding-top: 5rem;
+  padding-top: 8.5rem;
   margin-left: 15%;
   ${media.mobile} {
     text-align: center;
+    padding-top: 6rem;
     margin-left: 0%;
   }
+`;
+
+export const CarouselContainer = styled.div`
+  color: ${(props) => props.theme.color.secondary};
+  font-family: 'Poppins', sans-serif;
+  font-size: clamp(1.8rem, 3vw, 3rem);
+  padding-top: 8rem;
+  margin-left: 30%;
+  display: inline-flex;
+  align-items: center;
+  ${media.mobile} {
+    margin-left: 0%;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const slideUp = keyframes`
+  0% {
+    transform: translateY(100%);
+  } 
+  100% {
+    transform: translateY(0);
+  }
+`;
+
+export const CarouselRotator = styled.div`
+  font-weight: bold;
+  font-size: clamp(2.5rem, 5vw, 4rem);
+  border-bottom: solid 3.5px ${(props) => props.theme.color.secondary};
+  display: inline-block;
+  margin: auto;
+  overflow: hidden;
+  z-index: -1;
+`;
+
+export const CarouselRotatorItem = styled.div`
+  animation: ${slideUp} 1.5s;
 `;
--- components/Home/HomeAnimation.tsx
+++ components/Home/HomeAnimation.tsx
@@ -1,10 +1,36 @@
-import { AnimationContainer, LogoText } from './HomeAnimation.styled';
+import { useState, useEffect, useRef } from 'react';
+import {
+  AnimationContainer,
+  LogoText,
+  CarouselContainer,
+  CarouselRotator,
+  CarouselRotatorItem,
+} from './HomeAnimation.styled';
 
 export default function HomeAnimation() {
+  const [count, setCount] = useState(0);
+  const intervalRef: { current: NodeJS.Timeout | null } = useRef(null);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 2500);
+    return () => clearInterval(intervalRef.current as NodeJS.Timeout);
+  }, []);
+
   return (
     <>
       <AnimationContainer>
         <LogoText>Websolute</LogoText>
+        <CarouselContainer>
+          <span>IT Community</span>&nbsp;
+          <span>for</span>&nbsp;&nbsp;
+          <CarouselRotator>
+            {count % 3 === 0 && <CarouselRotatorItem>Hustler</CarouselRotatorItem>}
+            {count % 3 === 1 && <CarouselRotatorItem>Hacker</CarouselRotatorItem>}
+            {count % 3 === 2 && <CarouselRotatorItem>Hipster</CarouselRotatorItem>}
+          </CarouselRotator>
+        </CarouselContainer>
       </AnimationContainer>
     </>
   );
